7f6453fd1fdfac8e56a43e580abd842e1a34bddc
Henrique Portela <henriquepsoares@gmail.com>
2023-03-11 13:54:13 -0300

    Update HouseCard / Update Home

--- src/components/HouseCard.js
+++ src/components/HouseCard.js
@@ -1,26 +1,34 @@
 import React from 'react'
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
+import { useNavigate } from 'react-router';
 
 
-function HouseCard(props) {
+function HouseCard( {house} ) {
+  
+  const navigate = useNavigate();
+  
   return (
     <Card style={{ width: "15rem" }}>
-      <Card.Img style={{ height: "19np8.81px" }} variant="top" src={props.picture} />
-      <div className="card-title">
-            <h5>{props.name}</h5>
-        </div>
+      <Card.Img style={{ height: "19np8.81px" }} variant="top" src={house.picture} />
+
       <ListGroup className="list-group-flush">
         
-        <ListGroup.Item className="list-group-item card">{props.address.street}</ListGroup.Item>
-        <ListGroup.Item className="list-group-item card">{props.address.neighborhood}</ListGroup.Item>
-        <ListGroup.Item className="list-group-item card">{props.bedRooms}</ListGroup.Item>
-        <ListGroup.Item className="list-group-item card">{props.baths}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">Street: {house.address.street}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">Neighborhood: {house.address.neighborhood}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">City: {house.address.city}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">State: {house.address.state}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">Zip code: {house.address.zipCode}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">Bedrooms: {house.bedRooms}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">Baths: {house.baths}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">Year: {house.builtYear}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">Size: {house.houseSize}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">Price $: {house.price}</ListGroup.Item>
       </ListGroup>
       <div className="card-body">
-      
-   
-  </div>
+          <a href="#" className="card-link">Details</a> 
+      </div>
+
     </Card>
   
   )
