df806b1e5f24e7fc1fe93f7d3adff8d89e1d992e
Kenia Araujo <kenia.kfa@gmail.com>
2023-03-11 10:53:07 -0600

    Create BuyRoute

--- src/components/HouseCard.js
+++ src/components/HouseCard.js
@@ -3,19 +3,23 @@ import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
 
 
-function HouseCard(props) {
+function HouseCard(house) {
   return (
     <Card style={{ width: "15rem" }}>
-      <Card.Img style={{ height: "19np8.81px" }} variant="top" src={props.picture} />
-      <div className="card-title">
-            <h5>{props.name}</h5>
-        </div>
+      <Card.Img style={{ height: "19np8.81px" }} variant="top" src={house.picture} />
+      
+    
       <ListGroup className="list-group-flush">
         
-        <ListGroup.Item className="list-group-item card">{props.address.street}</ListGroup.Item>
-        <ListGroup.Item className="list-group-item card">{props.address.neighborhood}</ListGroup.Item>
-        <ListGroup.Item className="list-group-item card">{props.bedRooms}</ListGroup.Item>
-        <ListGroup.Item className="list-group-item card">{props.baths}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">{house.address.street}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">{house.address.neighborhood}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">{house.address.city}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">{house.address.state}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">{house.address.zipCode}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">BedRooms: {house.bedRooms}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">Build Yeras: {house.bedRooms}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card">Baths: {house.baths}</ListGroup.Item>
+        <ListGroup.Item className="list-group-item card"> ${house.price}</ListGroup.Item>
       </ListGroup>
       <div className="card-body">
       
